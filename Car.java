d4dfadbade3fad672896d3c9bb876f12f716ac31
lucasBarbeito <lucas.barbeito@greencodesoftware.com>
2024-03-11 15:49:19 -0300

    init commit

---
+++ Car.java
@@ -0,0 +1,29 @@
+class Car {
+
+
+    private String brand;
+    private String model;
+    private double maxSpeed;
+    private String color;
+
+    public Car (String brand, String model, double maxSpeed, String color){
+        this.brand = brand;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+        this.model = model;
+    }
+
+
+    public String getBrand() {
+        return brand;
+    }
+    public String getColor() {
+        return color;
+    }
+    public double getMaxSpeed() {
+        return maxSpeed;
+    }
+    public String getModel() {
+        return model;
+    }
+}
